5560a698a6f2ad516153d5fa393d56fd61bde361
SPARTAN-501 <luke.yin@alumni.ubc.ca>
2021-02-10 23:06:38 +0000

    Updated play to use client

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '135.23.222.131',
+    port: 50542,
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log('Successfully connected to game server');
+  });
+
+  conn.on('connect', () => {
+    conn.write('Name: LRY');
+  });
+
+  conn.on('data', (data) => {
+    console.log(data);
+  });
+
+  return conn;
+}
+
+module.exports = connect;
